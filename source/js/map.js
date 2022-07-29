63815526f57bc045fa44ff121cfb9856999a0431
Vladimir Trofimov <i_am@palant.org>
2022-07-29 13:40:37 +0300

    Adds index page

---
+++ source/js/map.js
@@ -0,0 +1,29 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('yandex-map', {
+            center: [59.938635, 30.323118],
+            zoom: 17,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Магазин «Мишка»',
+            balloonContent: 'Пароль — «Превед, медвед!»'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '../img/map-ballon.svg',
+            // Размеры метки.
+            iconImageSize: [67, 100],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [0, 0]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
+
