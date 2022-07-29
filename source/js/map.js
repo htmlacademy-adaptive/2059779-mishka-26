ef4198558b8f4d2ec0af9820c42275608cfe0641
Vladimir Trofimov <i_am@palant.org>
2022-07-29 14:02:14 +0300

    Adds paths fixes and webmanifest

--- source/js/map.js
+++ source/js/map.js
@@ -1,5 +1,5 @@
 ymaps.ready(function () {
-    var myMap = new ymaps.Map('yandex-map', {
+    var myMap = new ymaps.Map('yandexmap', {
             center: [59.938635, 30.323118],
             zoom: 17,
             controls: []
@@ -15,7 +15,7 @@ ymaps.ready(function () {
             // Необходимо указать данный тип макета.
             iconLayout: 'default#image',
             // Своё изображение иконки метки.
-            iconImageHref: '../img/map-ballon.svg',
+            iconImageHref: 'img/map-ballon.svg',
             // Размеры метки.
             iconImageSize: [67, 100],
             // Смещение левого верхнего угла иконки относительно
