62024e535ebf9329b4262360aa9c6811277232cb
Vladimir Trofimov <i_am@palant.org>
2022-08-20 18:11:42 +0300

    Fixes header media query

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -1,13 +1,16 @@
 let navMain = document.querySelector('.main-header__navigation');
-let navToggle = document.querySelector('.toggle__button');
+let navToggle = document.querySelector('.toggle');
+let navButton = document.querySelector('.toggle__button');
 
 navMain.classList.add('main-header__navigation--close');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-header__navigation--close')) {
+    navButton.classList.add('toggle__button--close')
     navMain.classList.remove('main-header__navigation--close');
     navMain.classList.add('main-header__navigation--open');
   } else {
+    navButton.classList.remove('toggle__button--close')
     navMain.classList.add('main-header__navigation--close');
     navMain.classList.remove('main-header__navigation--open');
   }
