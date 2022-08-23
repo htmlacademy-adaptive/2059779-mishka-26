2e7497b831b0d19188d86e3e8be2d7f691e2fb94
Vladimir Trofimov <i_am@palant.org>
2022-08-23 13:18:13 +0300

    Removes margins from blocks

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -1,17 +1,18 @@
-let navMain = document.querySelector('.main-navigation');
-let navToggle = document.querySelector('.toggle');
+let navMain = document.querySelector('.main-header__navigation');
+let navToggle = document.querySelector('.main-header__toggle');
 let navButton = document.querySelector('.toggle__button');
 
-navMain.classList.add('main-navigation--close');
+navMain.classList.add('main-header__navigation--close');
+navToggle.classList.add('main-header__toggle--open');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-navigation--close')) {
+  if (navMain.classList.contains('main-header__navigation--close')) {
     navButton.classList.add('toggle__button--close')
-    navMain.classList.remove('main-navigation--close');
-    navMain.classList.add('main-navigation--open');
+    navMain.classList.remove('main-header__navigation--close');
+    navMain.classList.add('main-header__navigation--open');
   } else {
     navButton.classList.remove('toggle__button--close')
-    navMain.classList.add('main-navigation--close');
-    navMain.classList.remove('main-navigation--open');
+    navMain.classList.add('main-header__navigation--close');
+    navMain.classList.remove('main-header__navigation--open');
   }
 });
