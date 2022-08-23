dcadc16d96e59721d75eaa95a0c6edf35e7dd2d9
Vladimir Trofimov <i_am@palant.org>
2022-08-23 13:53:40 +0300

    Remakes toggle button

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -1,5 +1,6 @@
 let navMain = document.querySelector('.main-header__navigation');
 let navToggle = document.querySelector('.main-header__toggle');
+let navToggleButton = document.querySelector('.toggle');
 let navButton = document.querySelector('.toggle__button');
 
 navMain.classList.add('main-header__navigation--close');
@@ -8,10 +9,12 @@ navToggle.classList.add('main-header__toggle--open');
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-header__navigation--close')) {
     navButton.classList.add('toggle__button--close')
+    navToggleButton.classList.add('toggle--close')
     navMain.classList.remove('main-header__navigation--close');
     navMain.classList.add('main-header__navigation--open');
   } else {
     navButton.classList.remove('toggle__button--close')
+    navToggleButton.classList.remove('toggle--close')
     navMain.classList.add('main-header__navigation--close');
     navMain.classList.remove('main-header__navigation--open');
   }
